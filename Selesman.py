d46422a2cee96daf1813c4ef3053ade26cfed173
Futao <futao.gao@qq.com>
2019-09-30 17:10:18 +0800

    完成了全部统计功能，表格样式仍未完成

--- Selesman.py
+++ Selesman.py
@@ -16,15 +16,16 @@ class Selesman(object):
         self.ru_si_shi_jian = None          # 入司时间
         self.kao_he_lei_xing = None         # 考核类型
         self.he_tong_lei_xing = None        # 合同类型
+        self.he_ding_shuo_ming = None       # 核定说明
 
     @property
     def month(self):
-        """考核月份"""
+        """获取考核月份"""
         return self._month
 
     @month.setter
     def month(self, value):
-        """考核月份"""
+        """设置考核月份"""
         if int(value) > 0 and int(value) <= 12:
             self._month = value
         else:
@@ -33,17 +34,17 @@ class Selesman(object):
 
     @property
     def year(self):
-        """今年的年份"""
+        """获取今年的年份"""
         return datetime.now().year
 
     @property
     def zhong_zhi(self):
-        """中心支公司"""
+        """设置中心支公司"""
         return self._zhong_zhi
 
     @zhong_zhi.setter
     def zhong_zhi(self, value):
-        """中心支公司"""
+        """获取中心支公司简称"""
         sql_str = f"SELECT [中心支公司简称] \
             FROM [中心支公司] \
             WHERE [中心支公司] = '{value}'"
@@ -52,7 +53,7 @@ class Selesman(object):
 
     @property
     def ji_gou_jian_cheng(self):
-        """机构"""
+        """获取机构简称"""
         sql_str = f"SELECT [机构简称] \
             FROM [机构] \
             WHERE [机构] = '{self.ji_gou}'"
@@ -61,12 +62,12 @@ class Selesman(object):
 
     @property
     def tuan_dui(self):
-        """销售团队"""
+        """获取销售团队简称"""
         return self._tuan_dui
 
     @tuan_dui.setter
     def tuan_dui(self, value):
-        """销售团队"""
+        """获取销售团队全称并转化为销售团队简称"""
         sql_str = f"SELECT [团队简称] \
             FROM [团队] \
             WHERE [团队] = '{value}'"
@@ -75,12 +76,12 @@ class Selesman(object):
 
     @property
     def zhi_ji(self):
-        """现任职级"""
+        """获取现任职级"""
         return self._zhi_ji
 
     @zhi_ji.setter
     def zhi_ji(self, value):
-        """现任职级"""
+        """设置现任职级"""
         if value == '(null)':
             self._zhi_ji = ''
         else:
@@ -88,12 +89,12 @@ class Selesman(object):
 
     @property
     def ru_si_zhi_ji(self):
-        """入司职级"""
+        """设置入司职级"""
         return self._ru_si_zhi_ji
 
     @ru_si_zhi_ji.setter
     def ru_si_zhi_ji(self, value):
-        """入司职级"""
+        """获取入司职级"""
         if value == '(null)':
             self._ru_si_zhi_ji = ''
         else:
@@ -101,7 +102,7 @@ class Selesman(object):
 
     @property
     def he_ding(self):
-        """是否参与核定"""
+        """判断是否参与核定"""
         if int(self.ru_si_shi_jian[5:7]) == int(self.month) \
            and int(self.ru_si_shi_jian[:4]) != self.year:
             return True
@@ -110,9 +111,10 @@ class Selesman(object):
 
     @property
     def shang_nian_bao_fei(self):
-        """上年保费"""
+        """从数据库中获取上年保费"""
         # 入司月份与考核月份相同（入司每满12个月）
         # 同时不是本年入司的员工
+        # 满足以上两点的人员才需要重新核定司龄工资
         if self.he_ding is True:
             start_date = f"{self.year-1}-{self.month:>02}"
             end_date = f"{self.year}-{(int(self.month)-1):>02}"
@@ -126,11 +128,11 @@ class Selesman(object):
             temp = self.cur.fetchone()
 
             if temp[0] is None:
-                self._shang_nian_bao_fei = ''
+                self._shang_nian_bao_fei = ''      # 如果没有查询到数据则返回空
             else:
                 self._shang_nian_bao_fei = temp[0]
         else:
-            self._shang_nian_bao_fei = ''
+            self._shang_nian_bao_fei = ''      # 如果不需要重新核定司龄工资则返回空
 
         if self._shang_nian_bao_fei == '':
             return self._shang_nian_bao_fei
@@ -139,7 +141,10 @@ class Selesman(object):
 
     @property
     def tong_qi_bao_fei(self):
-        """同期保费"""
+        """从数据库中获取同期保费"""
+        # 入司月份与考核月份相同（入司每满12个月）
+        # 同时不是本年入司的员工
+        # 满足以上两点的人员才需要重新核定司龄工资
         if self.he_ding is True:
 
             end_date = f"{self.year-1}-{(int(self.month)-1):>02}"
@@ -154,11 +159,11 @@ class Selesman(object):
             temp = self.cur.fetchone()
 
             if temp[0] is None:
-                self._tong_qi_bao_fei = ''
+                self._tong_qi_bao_fei = ''      # 如果没有查询到数据则返回空
             else:
                 self._tong_qi_bao_fei = temp[0]
         else:
-            self._tong_qi_bao_fei = ''
+            self._tong_qi_bao_fei = ''      # 如果不需要重新核定司龄工资则返回空
 
         if self._tong_qi_bao_fei == '':
             return self._tong_qi_bao_fei
@@ -167,18 +172,21 @@ class Selesman(object):
 
     @property
     def tong_bi(self):
-        """保费同比"""
+        """计算并返回保费同比"""
         if self.he_ding is True:
             print(self.xing_ming)
-            self._tong_bi = float(self.shang_nian_bao_fei) \
-                / float(self.tong_qi_bao_fei) - 1
+            if self.tong_qi_bao_fei == '':
+                return ''
+            else:
+                self._tong_bi = float(self.shang_nian_bao_fei) \
+                    / float(self.tong_qi_bao_fei) - 1
         else:
             self._tong_bi = ''
         return self._tong_bi
 
     @property
     def yuan_si_ling_gong_zi(self):
-        """原司龄工资"""
+        """获取原司龄工资"""
         he_ding_date = f"{datetime.now().year}-{(int(self.month)):>02}"
         sql_str = f"SELECT [司龄工资] \
             FROM [司龄工资] \
@@ -188,8 +196,8 @@ class Selesman(object):
         return self.cur.fetchone()[0]
 
     @property
-    def zheng_ti_tong_bi(self):
-        """分公司整体同比增长率"""
+    def zheng_ti_shang_nian_bao_fei(self):
+        """获取分公司整体的上年保费"""
         start_date = f"{self.year-1}-{self.month:>02}"
         end_date = f"{self.year}-{(int(self.month)-1):>02}"
         sql_str = f"SELECT SUM ([签单保费/批改保费]) \
@@ -197,8 +205,11 @@ class Selesman(object):
             WHERE [投保确认日期] >= '{start_date}' \
             AND [投保确认日期] <= '{end_date}'"
         self.cur.execute(sql_str)
-        shang_nian_bao_fei = self.cur.fetchone()[0]
+        return self.cur.fetchone()[0]
 
+    @property
+    def zheng_ti_tong_qi_bao_fei(self):
+        """获取分公司整体的同期保费"""
         start_date = f"{self.year-2}-{self.month:>02}"
         end_date = f"{self.year-1}-{(int(self.month)-1):>02}"
         sql_str = f"SELECT SUM ([签单保费/批改保费]) \
@@ -206,14 +217,55 @@ class Selesman(object):
             WHERE [投保确认日期] >= '{start_date}' \
             AND [投保确认日期] <= '{end_date}'"
         self.cur.execute(sql_str)
-        tong_qi_bao_fei = self.cur.fetchone()[0]
+        return self.cur.fetchone()[0]
 
-        return shang_nian_bao_fei / tong_qi_bao_fei - 1
+    @property
+    def zheng_ti_tong_bi(self):
+        """分公司整体同比增长率"""
+        return (float(self.zheng_ti_shang_nian_bao_fei) /
+                float(self.zheng_ti_tong_qi_bao_fei) - 1)
 
     @property
     def si_ling_gong_zi_bian_hua(self):
-        sql_str = f""
-        if he_ding is False:
+        """计算司龄工资的变化情况"""
+        if self.he_ding is False:
+            self.he_ding_shuo_ming = '距上次核定或入职未满一年，不予重新核定'
             return 0
+        elif self.kao_he_lei_xing == '团队经理':
+            if self.yuan_si_ling_gong_zi >= 1000:
+                self.he_ding_shuo_ming = '司龄工资已达上限，不再增加'
+                return 0
+            else:
+                self.he_ding_shuo_ming = '团队经理司龄工资按入职时间每年递增100元'
+                return 100
         elif int(self.ru_si_shi_jian[:4]) == self.year - 1:
-            pass
+            sql_str = f"SELECT [职级级别] \
+                FROM [销售人员] \
+                JOIN [客户经理] \
+                ON [销售人员].[入司职级] = [客户经理].[职级] \
+                WHERE  [业务员] = '{self.ye_wu_yuan}'"
+            self.cur.execute(sql_str)
+            ji_bie = self.cur.fetchone()[0]
+            if ji_bie <= 6:
+                self.he_ding_shuo_ming = '入司首次满一年，且入司职级为初级客户经理，司龄工资为50元'
+                return 50
+            else:
+                self.he_ding_shuo_ming = '入司首次满一年，且入司职级为中级或以上客户经理，司龄工资为100元'
+                return 100
+        else:
+            if self.tong_bi > 0 and self.tong_bi >= self.zheng_ti_tong_bi:
+                self.he_ding_shuo_ming = '入司超过一年，同比增长率大于分公司，司龄工资增加100元'
+                return 100
+            elif self.tong_bi > 0:
+                self.he_ding_shuo_ming = '入司超过一年，同比增长率大于零但小于分公司，司龄工资增加50元'
+                return 50
+            else:
+                self.he_ding_shuo_ming = '入司超过一年，同比增长率小于0，司龄工资减50元'
+                return - 50
+
+    @property
+    def xin_si_ling_gong_zi(self):
+        value = self.yuan_si_ling_gong_zi + self.si_ling_gong_zi_bian_hua
+        if value < 0:
+            value = 0
+        return value
--- write_excel.py
+++ write_excel.py
@@ -1,11 +1,56 @@
 from openpyxl import Workbook
+from openpyxl.styles import (Border,
+                             Font,
+                             NamedStyle,
+                             PatternFill,
+                             Side,
+                             Alignment)
 
 
 def write_excel(info):
     wb = Workbook()
     ws = wb.active
     ws.title = "前线人员司龄工资信息表"
+
+    font_bold = Font(name='微软雅黑',
+                     size=12,
+                     bold=True)
+    font = Font(name='微软雅黑', 
+                size=12)
+    border = Border(left=Side(style='thin', color='000000'),
+                    right=Side(style='thin', color='000000'),
+                    top=Side(style='thin', color='000000'),
+                    bottom=Side(style='thin', color='000000'))
+    fill = PatternFill(fill_type=None,
+                       bgColor='CCCCCC')
+    alignment = Alignment(horizontal='center',
+                          vertical='center',
+                          wrap_text=True)
+    style_bold = NamedStyle(name='style_bold',
+                            font=font_bold,
+                            border=border,
+                            alignment=alignment)
+    style_no_fill = NamedStyle(name='sytle_no_fill',
+                               font=font,
+                               border=border,
+                               alignment=alignment)
+    style_fill = NamedStyle(name='style_fill',
+                            font=font,
+                            border=border,
+                            fill=fill,
+                            alignment=alignment)
+
+    wb.add_named_style(style_bold)
+    wb.add_named_style(style_no_fill)
+    wb.add_named_style(style_fill)
+
     nrow = 1
+    ws.cell(row=nrow, column=1).value = '前线人员司龄工作核定表'
+    ws.cell(row=nrow, column=1).font = font_bold
+    ws.cell(row=nrow, column=1).alignment = alignment
+    ws.merge_cells('A1:O1')
+
+    nrow += 1
     ws.cell(row=nrow, column=1).value = '序号'
     ws.cell(row=nrow, column=2).value = '机构'
     ws.cell(row=nrow, column=3).value = '团队'
@@ -14,17 +59,37 @@ def write_excel(info):
     ws.cell(row=nrow, column=6).value = '现任职级'
     ws.cell(row=nrow, column=7).value = '入司职级'
     ws.cell(row=nrow, column=8).value = '入司时间'
-    ws.cell(row=nrow, column=9).value = '18年签单保费'
-    ws.cell(row=nrow, column=10).value = '17年签单保费'
+    ws.cell(row=nrow, column=9).value = '滚动12个月签单保费'
+    ws.cell(row=nrow, column=10).value = '滚动12个月同期签单保费'
     ws.cell(row=nrow, column=11).value = '同比增长率'
     ws.cell(row=nrow, column=12).value = '现任司龄工资'
     ws.cell(row=nrow, column=13).value = '司龄工资变化'
     ws.cell(row=nrow, column=14).value = '考核后司龄工资'
+    ws.cell(row=nrow, column=15).value = '核定说明'
+
+    # ws.column_dimensions['A'] = 30
+    # ws.column_dimensions['B'] = 30
+    # ws.column_dimensions['C'] = 30
+    # ws.column_dimensions['D'] = 30
+    # ws.column_dimensions['E'] = 30
+    # ws.column_dimensions['F'] = 30
+    # ws.column_dimensions['G'] = 30
+    # ws.column_dimensions['H'] = 30
+    # ws.column_dimensions['I'] = 30
+    # ws.column_dimensions['J'] = 30
+    # ws.column_dimensions['K'] = 30
+    # ws.column_dimensions['L'] = 30
+    # ws.column_dimensions['M'] = 30
+    # ws.column_dimensions['N'] = 30
+    # ws.column_dimensions['O'] = 30
+
+    for i in range(1, 16):
+        ws.cell(row=nrow, column=i).style = style_bold
 
     nrow += 1
 
     for ren in info:
-        ws.cell(row=nrow, column=1).value = nrow - 1
+        ws.cell(row=nrow, column=1).value = nrow - 2
         ws.cell(row=nrow, column=2).value = ren.zhong_zhi
         ws.cell(row=nrow, column=3).value = ren.tuan_dui
         ws.cell(row=nrow, column=4).value = ren.xing_ming
@@ -36,8 +101,17 @@ def write_excel(info):
         ws.cell(row=nrow, column=10).value = ren.tong_qi_bao_fei
         ws.cell(row=nrow, column=11).value = ren.tong_bi
         ws.cell(row=nrow, column=12).value = ren.yuan_si_ling_gong_zi
-        ws.cell(row=nrow, column=13).value = '司龄工资变化'
-        ws.cell(row=nrow, column=14).value = '考核后司龄工资'
+        ws.cell(row=nrow, column=13).value = ren.si_ling_gong_zi_bian_hua
+        ws.cell(row=nrow, column=14).value = ren.xin_si_ling_gong_zi
+        ws.cell(row=nrow, column=15).value = ren.he_ding_shuo_ming
+        if nrow % 2 == 1:
+            style = style_fill
+        else:
+            style = style_no_fill
+
+        for i in range(1, 16):
+            ws.cell(row=nrow, column=i).style = style
+
         nrow += 1
 
     wb.save('司龄工资考核表.xlsx')
